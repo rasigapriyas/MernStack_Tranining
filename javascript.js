ac480467c818106eda164b786fcebb044165e009
rasigapriyas <rasigapriyaselvaraj17@gmail.com>
2025-10-10 22:56:13 +0530

    Initial project upload

---
+++ javascript.js
@@ -0,0 +1,19 @@
+// console.log("testing");
+// console.log(a);
+// let a=6;
+
+// let object1={age:33};
+// let object2=object1;
+// object1.age=22;
+// console.log(object2.age);
+
+function sum(a,b)
+{
+  console.log(a+b);
+}
+sum(11,22);
+// in javascript use const instead of fun and use = before and use arrow symbol
+const sum2=(a,b,c)=>{
+  console.log(a+b+c);
+}
+sum(11,22,33);
